6847c62e870ebf141cd884feb96be3332cdab753
Maxime Pages <maximepag02@gmail.com>
2025-05-15 11:34:10 +0200

    CLI

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "RiscCPU/core"
+import (
+	"RiscCPU/core"
+	"fmt"
+	"os"
+	"path"
+)
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <filename> [-debug]")
+		return
+	}
+
 	cpu := core.NewCPU(core.NewMemory())
-	cpu.ExecuteFile("output.exe")
+	core.Kernel.CWD = path.Dir(os.Args[1])
+	if os.Args[2] == "-debug" {
+		cpu.DebugFile(os.Args[1])
+	} else {
+		cpu.LoadFile(os.Args[1])
+	}
 }
